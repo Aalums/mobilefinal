a4092bd5138948e81d7e53bb9941c0de281c7962
aalums <59070194@it.kmitl.ac.th>
2018-12-11 10:17:30 +0700

    Start

---
+++ app/src/main/java/th/ac/kmitl/a59070194/LoginFragment.java
@@ -0,0 +1,43 @@
+package th.ac.kmitl.a59070194;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initRegBTN();
+
+    }
+
+    void initRegBTN () {
+        Button _regBTN = getView().findViewById(R.id.login_regBTN);
+        _regBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+                Log.d("LOGIN", "GOTO REGISTER");
+            }
+        });
+    }
+}
